dec97ea051d31fa4a21bdb0efeb38fc27168316e
carloslg32 <carloslg3282@gmail.com>
2023-09-21 21:26:00 -0300

    Initial commit

---
+++ practica b.js/script.js
@@ -0,0 +1,22 @@
+function operacionesMatematica (a,b) {
+    let resultado = {
+        suma: a + b,
+        resta: a - b,
+        multiplicacion: a * b,
+        division a / b,
+
+    };
+    return resultado; 
+}
+let resultado = operacionesMatematicas (2 * 2);
+console.log(resultado);
+
+function suma(e,d){
+let resultado2 ={
+    suma: e+d
+};
+return resultado2;
+
+}
+let resultado2 = suma(3,5)
+console.log(resultado2)
